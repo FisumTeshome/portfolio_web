ad922381fb745fa17a249fd7940eb09bb723c682
Fitsum Teshome <teshfitsum834@gmail.com>
2025-09-15 23:47:35 +0300

    Updating my portfolio:Navbar,nameSection and some css and styles

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
-<script setup lang="ts">
-import Home from './pages/Home.vue'
-</script>
+
 
 <template>
-  <Home />
+  <div class="bg-[#111827] min-h-screen">
+    <Navbar />
+    <nameSection />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+// import Home from './pages/Home.vue'
+import { defineAsyncComponent } from 'vue';
+// importing components assyncronusly
+const Navbar = defineAsyncComponent(() => import('./components/Navbar.vue'));
+const nameSection = defineAsyncComponent(() => import('./components/namezSection.vue'));
+</script>
+
