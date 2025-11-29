fb7597e54d8268f744d7d818a5fe49a6831eb181
Fitsum Teshome <teshfitsum834@gmail.com>
2025-11-29 05:56:46 +0300

    updating vit.config to include vercel setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-export default defineConfig({
-  base: '/portfolio_web/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src'
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: mode === 'github' ? '/portfolio_web/' : '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     }
   }
 })
